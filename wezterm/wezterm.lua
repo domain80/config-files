7e509165110c753210538fffed7dc45d7fa5b9b6
domain80 <dmainoo@bitovi.com>
2026-06-03 12:42:43 +0000

    add dynamic light/dark mode color scheme switching

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,9 +4,34 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- Automatically switch color scheme when macOS appearance changes
+local function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "GitHub Dark"
+  else
+    return "One Light (base16)"
+  end
+end
+
+wezterm.on("window-config-reloaded", function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
+end)
+
+if wezterm.gui then
+  config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+else
+  config.color_scheme = "GitHub Dark"
+end
+
 -- Window size (~16:10 aspect ratio)
-config.initial_cols = 110
-config.initial_rows = 17
+config.initial_cols = 135
+config.initial_rows = 24
 
 -- Font
 config.font = wezterm.font("JetBrainsMono Nerd Font")
