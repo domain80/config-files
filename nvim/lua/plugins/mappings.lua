d8cfa15edb9646226b3d93345ba5d39a83a03b0f
domain80 <dev.davidmain00@gmail.com>
2026-07-03 12:52:04 +0000

    auto: config sync (domain80-2) 2026-07-03 12:52:04

--- nvim/lua/plugins/mappings.lua
+++ nvim/lua/plugins/mappings.lua
@@ -1,33 +1,11 @@
+-- Motion keymaps are derived from the shared editing source so that full
+-- Neovim and vscode-neovim can never drift. See lua/shared/editing.lua.
+local editing = require "shared.editing"
+
+---@type LazySpec
 return {
-  {
-    "AstroNvim/astrocore",
-    opts = {
-      mappings = {
-        n = {
-          -- Make H go to first non-blank character (originally ^)
-          ["H"] = { "^", desc = "Go to first non-blank character" },
-          -- Make ^ go to top of screen (originally H)
-          ["^"] = { "H", desc = "Go to top of screen" },
-          -- Make L go to end of line (originally $)
-          ["L"] = { "$", desc = "Go to end of line" },
-          -- Make $ go to bottom of screen (originally L)
-          ["$"] = { "L", desc = "Go to bottom of screen" },
-        },
-        v = {
-          -- Apply the same swaps in visual mode
-          ["H"] = { "^", desc = "Go to first non-blank character" },
-          ["^"] = { "H", desc = "Go to top of screen" },
-          ["L"] = { "$", desc = "Go to end of line" },
-          ["$"] = { "L", desc = "Go to bottom of screen" },
-        },
-        o = {
-          -- Apply in operator-pending mode for motions like dH, d^, dL, d$
-          ["H"] = { "^", desc = "Go to first non-blank character" },
-          ["^"] = { "H", desc = "Go to top of screen" },
-          ["L"] = { "$", desc = "Go to end of line" },
-          ["$"] = { "L", desc = "Go to bottom of screen" },
-        },
-      },
-    },
+  "AstroNvim/astrocore",
+  opts = {
+    mappings = editing.astrocore_mappings(),
   },
 }
