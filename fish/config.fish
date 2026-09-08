4dd9ed4c6ee7c66a8486715afeb4a3ba9ce50aeb
domain80 <dmainoo@bitovi.com>
2026-09-08 16:30:30 +0000

    auto: config sync (Davids-MacBook-Pro-16) 2026-09-08 16:30:30

--- fish/config.fish
+++ fish/config.fish
@@ -28,6 +28,7 @@ abbr --set-cursor=@\$ -a tn cd ~/dev/terydin@\$
 abbr --set-cursor=@\$ -a pb cd ~/Documents/obsidian/Playbook2026/@\$
 abbr --set-cursor=@\$ -a me cd ~/dev/me@\$
 abbr --set-cursor=@\$ -a bi cd ~/dev/bitovi@\$
+abbr --set-cursor=@\$ -a yu cd ~/dev/bitovi@\$
 # abbr end
 
 # bun
