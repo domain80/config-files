8197bb66f8804d3ede6370dc11a6cfc8a8839e4e
domain80 <dmainoo@bitovi.com>
2026-09-17 15:07:27 +0000

    auto: config sync (Davids-MacBook-Pro-16) 2026-09-17 15:07:27

--- nvim/lua/shared/editing.lua
+++ nvim/lua/shared/editing.lua
@@ -23,6 +23,7 @@ M.keymaps = {
   { { "n", "v", "o" }, "^", "H", "Go to top of screen" },
   { { "n", "v", "o" }, "L", "$", "Go to end of line" },
   { { "n", "v", "o" }, "$", "L", "Go to bottom of screen" },
+  { { "n", "v", "o" }, "U", "ctrl", "r", "Redo" },
 }
 
 -- Engine-level options that both editors honour. Add to this as you adopt more.
